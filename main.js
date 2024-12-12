bb498610217ddfec77b79b606e7321e04ec685b8
kzoeps <kzoepa@gmail.com>
2024-12-11 22:14:50 -0500

    fix: update types for population by categories

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+let map; // Declare map as a global variable
+let geoLayer; // To store and remove the existing GeoJSON layer
+let legend; // To store and remove the existing legend
 // Navigation data structure
+const POPULATION_SUBCATEGORIES = {
+    totalPopulation: 'total-population',
+    malePopulation: 'male-population',
+    femalePopulation: 'femail-population',
+    malePopulationPercentage: 'male-population-percentage',
+    femalePopulationPercentage: 'female-population-percentage'
+}
 const mapDataCategories = [
     {
         label: 'Population',
@@ -7,15 +17,23 @@ const mapDataCategories = [
         subsections: [
             {
                 label: 'Total Population',
-                id: 'total-population',
+                id: POPULATION_SUBCATEGORIES.totalPopulation,
             },
             {
                 label: "Population By Male",
-                id: "male-population"
+                id: POPULATION_SUBCATEGORIES.malePopulation
             },
             {
                 label: "Population By Female",
-                id: "female-population"
+                id: POPULATION_SUBCATEGORIES.femalePopulation
+            },
+            {
+                label: "Male Population Percentage",
+                id: POPULATION_SUBCATEGORIES.malePopulationPercentage
+            },
+            {
+                label: "Female Population Percentage",
+                id: POPULATION_SUBCATEGORIES.femalePopulationPercentage
             }
         ]
     },
@@ -73,7 +91,7 @@ function createNavigation() {
                 }
                 activeSubCategory = subsectionItem;
                 subsectionItem.classList.add('active');
-                loadData(categoryData.dataPath)
+                loadData(categoryData.dataPath, subData.id)
             })
             subsectionsContainer.appendChild(subsectionItem);
         });
@@ -85,33 +103,88 @@ function createNavigation() {
     mapContainer.style.position = 'relative';
     mapContainer.appendChild(navContainer);
 
+
 }
 
-function loadData(dataPath = 'data/dzongkhag-population.json') {
+function getPopulationColors(d, selectedSubcategory) {
+    const numbersWise = d > 100000 ? '#800026' :
+        d > 60000 ? '#BD0026' :
+            d > 35000 ? '#E31A1C' :
+                d > 15000 ? '#FC4E2A' :
+                    d > 5000 ? '#FD8D3C' :
+                        d > 2000 ? '#FEB24C' :
+                            '#FFEDA0';
+    const percentageWise = d > 90 ? '#800026' :
+        d > 80 ? '#BD0026' :
+            d > 70 ? '#E31A1C' :
+                d > 60 ? '#FC4E2A' :
+                    d > 50 ? '#FD8D3C' :
+                        d > 40 ? '#FEB24C' :
+                            d > 30 ? '#FED976' :
+                                d > 20 ? '#FFEDA0' :
+                                    d > 10 ? '#FFFFCC' :
+                                        '#FFFFFF';
+    switch (selectedSubcategory) {
+        case POPULATION_SUBCATEGORIES.totalPopulation:
+            return numbersWise
+        case POPULATION_SUBCATEGORIES.malePopulation:
+            return numbersWise
+        case POPULATION_SUBCATEGORIES.femalePopulation:
+            return numbersWise
+        case POPULATION_SUBCATEGORIES.malePopulationPercentage:
+            return percentageWise
+
+    }
+}
+
+const PERCENTAGE_WISE_SUBCATEGORIES = [POPULATION_SUBCATEGORIES.malePopulationPercentage, POPULATION_SUBCATEGORIES.femalePopulationPercentage];
+
+const formatNumber = (number) => {
+    const userLocale = window.navigator.language;
+    return new Intl.NumberFormat(userLocale).format(number);
+}
+
+function loadData(dataPath = 'data/dzongkhag-population.json', selectedSubcategory = 'total-population') {
+    // Remove existing map if it exists
+    if (map) {
+        map.remove();
+    }
+
     Promise.all([
         d3.json("gadm41_BTN_1.json"),
-        d3.json("data/dzongkhag-population.json")
+        d3.json(dataPath)
     ])
         .then(([geoJsonData, populationData]) => {
             createNavigation();
-            const map = L.map('map').setView([27.5142, 90.4336], 8);
+            // Reinitialize map
+            map = L.map('map').setView([27.5142, 90.4336], 8);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
             // Function to get color based on population
             function getColor(d) {
-                return d > 100000 ? '#800026' :
-                    d > 60000 ? '#BD0026' :
-                        d > 35000 ? '#E31A1C' :
-                            d > 15000 ? '#FC4E2A' :
-                                d > 5000 ? '#FD8D3C' :
-                                    d > 2000 ? '#FEB24C' :
-                                        '#FFEDA0';
+                return getPopulationColors(d, selectedSubcategory);
             }
 
             // Style function for GeoJSON layer
             function style(feature) {
+                // Modify this based on the selected subcategory
+                let populationValue;
+                switch (selectedSubcategory) {
+                    case POPULATION_SUBCATEGORIES.malePopulation:
+                        populationValue = populationData[feature.properties.NAME_1]?.["Male"];
+                        break;
+                    case POPULATION_SUBCATEGORIES.femalePopulation:
+                        populationValue = populationData[feature.properties.NAME_1]?.["Female"];
+                        break;
+                    case POPULATION_SUBCATEGORIES.malePopulationPercentage:
+                        populationValue = formatNumber(populationData[feature.properties.NAME_1]?.["Male"] / populationData[feature.properties.NAME_1]?.["Both Sex"] * 100);
+                        break;
+                    default:
+                        populationValue = populationData[feature.properties.NAME_1]?.["Both Sex"];
+                }
+
                 return {
-                    fillColor: getColor(populationData[feature.properties.NAME_1]?.["Both Sex"]),
+                    fillColor: getColor(populationValue),
                     weight: 2,
                     opacity: 1,
                     color: 'white',
@@ -119,20 +192,38 @@ function loadData(dataPath = 'data/dzongkhag-population.json') {
                     fillOpacity: 0.7
                 };
             }
-            const legend = L.control({ position: 'bottomright' });
+
+            // Remove existing legend if it exists
+            if (legend) {
+                map.removeControl(legend);
+            }
+
+            // Create new legend
+            legend = L.control({ position: 'bottomright' });
 
             legend.onAdd = function (map) {
                 const div = L.DomUtil.create('div', 'info legend');
                 const populations = [0, 1000, 2000, 5000, 10000, 20000, 50000, 100000];
+                const percentages = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+                // Update legend title based on subcategory
+                const legendTitles = {
+                    [POPULATION_SUBCATEGORIES.totalPopulation]: 'Total Population',
+                    [POPULATION_SUBCATEGORIES.malePopulation]: 'Male Population',
+                    [POPULATION_SUBCATEGORIES.femalePopulation]: 'Female Population',
+                    [POPULATION_SUBCATEGORIES.malePopulationPercentage]: "Male Population By Percentage",
+                    [POPULATION_SUBCATEGORIES.femalePopulationPercentage]: "Female Population By Percentage"
+                };
 
                 // Legend title
-                div.innerHTML += '<h4>Population</h4>';
+                div.innerHTML += `<h4>${legendTitles[selectedSubcategory]}</h4>`;
 
                 // Loop through population intervals and generate a label with a colored square for each interval
-                for (let i = 0; i < populations.length; i++) {
+                const loopOver = PERCENTAGE_WISE_SUBCATEGORIES.includes(selectedSubcategory) ? percentages : populations;
+                for (let i = 0; i < loopOver.length; i++) {
                     div.innerHTML +=
-                        '<i style="background:' + getColor(populations[i] + 1) + '"></i> ' +
-                        populations[i] + (populations[i + 1] ? '&ndash;' + populations[i + 1] + '<br>' : '+');
+                        '<i style="background:' + getColor(loopOver[i] + 1) + '"></i> ' +
+                        loopOver[i] + (loopOver[i + 1] ? '&ndash;' + loopOver[i + 1] + '<br>' : '+');
                 }
 
                 return div;
@@ -140,13 +231,39 @@ function loadData(dataPath = 'data/dzongkhag-population.json') {
 
             legend.addTo(map);
 
-            // Add GeoJSON data to the map
-            L.geoJSON(geoJsonData, {
+            // Remove existing GeoJSON layer if it exists
+            if (geoLayer) {
+                map.removeLayer(geoLayer);
+            }
+
+            // Add new GeoJSON data to the map
+            geoLayer = L.geoJSON(geoJsonData, {
                 style: style,
                 onEachFeature: function (feature, layer) {
+                    // Determine which population to display in tooltip
+                    let populationValue;
+                    let populationLabel;
+                    switch (selectedSubcategory) {
+                        case POPULATION_SUBCATEGORIES.malePopulation:
+                            populationValue = formatNumber(populationData[feature.properties.NAME_1]?.["Male"]);
+                            populationLabel = "Male Population"
+                            break;
+                        case POPULATION_SUBCATEGORIES.femalePopulation:
+                            populationValue = formatNumber(populationData[feature.properties.NAME_1]?.["Female"]);
+                            populationLabel = "Female Population"
+                            break;
+                        case POPULATION_SUBCATEGORIES.malePopulationPercentage:
+                            populationValue = formatNumber(populationData[feature.properties.NAME_1]?.["Male"] / populationData[feature.properties.NAME_1]?.["Both Sex"] * 100);
+                            populationLabel = "Male Population Percentage"
+                            break
+                        default:
+                            populationValue = formatNumber(populationData[feature.properties.NAME_1]?.["Both Sex"]);
+                            populationLabel = "Total Population"
+                    }
+
                     layer.bindTooltip(`
                         <strong>${feature.properties.NAME_1}</strong><br>
-                        Population: ${populationData[feature.properties.NAME_1]?.["Both Sex"]}
+                    ${populationLabel}: ${populationValue}
                     `, {
                         permanent: false,
                         direction: 'right',
@@ -160,4 +277,4 @@ function loadData(dataPath = 'data/dzongkhag-population.json') {
         });
 }
 
-loadData();
+loadData()
